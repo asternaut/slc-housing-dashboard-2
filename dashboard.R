a3938b10878349f485f398009fecd36b5e805864
Daniel Hadley <dphnrome@gmail.com>
2017-10-27 13:17:16 -0600

    added widgets

--- dashboard.R
+++ dashboard.R
@@ -8,20 +8,49 @@ setwd("~/Google Drive File Stream/My Drive/SI/DataScience/Side projects/SLC Hous
 project_con <- read_excel("Housing Database Combined Data.xlsx", sheet = "All Data")
 MSA_unemployment <- read_excel("MSA-unemployment.xlsx", sheet = 1)
 
-ui <- dashboardPage(
-  dashboardHeader(title="SLC Housing Dashboard"),
-  dashboardSidebar(),
-  dashboardBody(
-    fluidRow(
-      box(highchartOutput("plot1", height = 250)),
-      box(plotOutput("plot2", height=250))
+#### UI ####
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+             badgeLabel = "new", badgeColor = "green")
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              box(highchartOutput("plot1", height = 250)),
+              box(plotOutput("plot2", height=250))
+            ),
+            fluidRow(
+              tags$iframe(src = "http://slcgov.maps.arcgis.com/apps/PublicInformation/index.html?appid=f632417a8bd94d5eb04f1f4eea728ce6", seamless=NA, height = 400, width = "100%")
+            )
     ),
-    fluidRow(
-      tags$iframe(src = "http://slcgov.maps.arcgis.com/apps/PublicInformation/index.html?appid=f632417a8bd94d5eb04f1f4eea728ce6", seamless=NA, height = 400, width = "100%")
+    
+    tabItem(tabName = "widgets",
+            fluidRow(
+              # A static infoBox
+              infoBox("New Affordable Units", 10 * 2, icon = icon("credit-card")),
+              # Dynamic infoBoxes
+              infoBox("New Market Units", 10 * 2, icon = icon("credit-card")),
+              infoBox("New Dashboards", 1, icon = icon("dashboard"))
+            )
     )
   )
 )
 
+# Put them together into a dashboardPage
+ui <- dashboardPage(
+  dashboardHeader(title = "SLC Housing"),
+  sidebar,
+  body
+)
+
+
+
+#### Server ####
 server <- function(input, output) {
   output$plot1<-renderHighchart({
     barchart<-hchart(project_con$`Type (PSH, Affordable, or Market`, 
