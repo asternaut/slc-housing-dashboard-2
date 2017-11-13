cc082d5ea3d87f7b129a65bbf32c0b78199dcd0a
Suyash Thite <suyash@Suyashs-MacBook-Pro.local>
2017-11-12 21:15:20 -0700

    TidyCensus Maps updated and Git Ignore added

--- tidycensus.R
+++ tidycensus.R
@@ -3,21 +3,60 @@ library("tidycensus")
 library("ggplot2")
 library("sf")
 library("viridis")
+library("leaflet")
+library("stringr")
 
 census_api_key("d70672fd4eb7640a7bc59b0dbdfa3578a7fc0040")
+####code for rent
+utah_rent <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
+                  variables = "B25111_001E", geometry = TRUE)
 
-utah <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
-                  variables = "B19013_001E", geometry = TRUE)
+pal <- colorNumeric(palette = "magma", 
+                    domain = utah_rent$estimate)
 
-#View(utah)
+utah_rent %>%
+  st_transform(crs = "+init=epsg:4326") %>%
+  leaflet(width = "100%") %>%
+  addProviderTiles(provider = "CartoDB.Positron") %>%
+  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
+              stroke = FALSE,
+              smoothFactor = 0,
+              fillOpacity = 0.7,
+              color = ~ pal(estimate)) %>%
+  addLegend("bottomright", 
+            pal = pal, 
+            values = ~ estimate,
+            title = "Median Gross Rent",
+            labFormat = labelFormat(prefix = "$"),
+            opacity = 1)
 
-utah %>%
-  ggplot(aes(fill = estimate, color = estimate)) + 
-  geom_sf() + 
-  coord_sf(crs = 26911) + 
-  scale_fill_viridis(option = "magma") + 
-  scale_color_viridis(option = "magma")
 
+utah_rent <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
+                     variables = "B25111_001E", geometry = TRUE)
 
+pal <- colorNumeric(palette = "magma", 
+                    domain = utah_rent$estimate)
+###Code for Home Value
+utah_home <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
+                     variables = "B25077_001", geometry = TRUE)
 
+pal <- colorNumeric(palette = "magma", 
+                    domain = utah_home$estimate)
 
+utah_home %>%
+  st_transform(crs = "+init=epsg:4326") %>%
+  leaflet(width = "100%") %>%
+  addProviderTiles(provider = "CartoDB.Positron") %>%
+  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
+              stroke = FALSE,
+              smoothFactor = 0,
+              fillOpacity = 0.7,
+              color = ~ pal(estimate)) %>%
+  addLegend("bottomright", 
+            pal = pal, 
+            values = ~ estimate,
+            title = "Median Home Value",
+            labFormat = labelFormat(prefix = "$"),
+            opacity = 1)
+#v15 <- load_variables(2015, "acs5", cache = TRUE)
+#View(v15)
