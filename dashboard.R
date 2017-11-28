98e7a57d367c6027bf534609d916708fea78952e
Suyash Thite <suyash@Suyashs-MacBook-Pro.local>
2017-11-27 19:12:13 -0700

    Suyash Merge 11/26

--- dashboard.R
+++ dashboard.R
@@ -3,36 +3,19 @@ library(shinydashboard)
 library(tidyverse)
 library(readxl)
 library(highcharter)
-library("tidycensus")
-library("ggplot2")
-library("sf")
-library("viridis")
 library("leaflet")
-library("stringr")
-source("tidycensus.R")
-
-#setwd("~/")
-
-setwd("/Users/suyash/Sorenson/SLC-Housing-Dashboard")
-
-
 
 project_con <- read_excel("Data/Housing Database Combined Data.xlsx", sheet = "All Data 2")
 MSA_unemployment <- read_excel("Data/MSA-unemployment.xlsx", sheet = "DataByYear")
 Permit <- read_excel("Data/Permit_adjusted.xlsx", sheet = "State Total")
 
 #### UI ####
-
-
-tags$head(
-  tags$link(rel = "stylesheet", type = "text/css", href = "test.css")
-)
-
-
+fluidPage(theme = "test.css",
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Welcome", tabName = "welcome", icon = icon("home")),
     menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("How did we get here?", tabName = "how", icon = icon("bar-chart")),
     menuItem("Goals of Growing SLC", tabName = "goals", icon = icon("road")),
     menuItem("Widgets", icon = icon("th"), tabName = "widgets",
              badgeLabel = "new", badgeColor = "green"),
@@ -40,7 +23,7 @@ sidebar <- dashboardSidebar(
     menuItem("Help",icon = icon("info-circle"))
     
   )
-)
+),
 
 body <- dashboardBody(
   # Dashboard favicon and title
@@ -51,53 +34,97 @@ body <- dashboardBody(
   
   tabItems(
     tabItem(
-       tabName="welcome",
-       fluidRow(
-         box(title = "", status = "primary", width = 8, 
-             img(src = "house.png",
-                 height = 64,
-                 width = 64
-              ),
-             h2("SLC Housing Dashboard"),
-             h4("Dynamic Web-based Analytics for Salt Lake City Housing"),
-             br(),
-             h4("SLC Housing is a ",a(href = 'http://shiny.rstudio.com', 'Shiny'),"web application built on top of R for housing-related data analytics"),
-             br(),
-
-             h4(HTML('&copy'), ' 2017 by Sorenson Impact Center at the University of Utah')
-
-         ),
-         uiOutput("projectBox"),
-         uiOutput("companyBox"),
-         uiOutput("houseBox")
-       )
-     ),
+      tabName="welcome",
+      fluidRow(
+        box(title = "", status = "primary", width = 8, 
+            img(src = "house.png",
+                height = 64,
+                width = 64
+            ),
+            h2("SLC Housing Dashboard"),
+            h4("Dynamic Web-based Analytics for Salt Lake City Housing"),
+            br(),
+            h4("SLC Housing is a ",a(href = 'http://shiny.rstudio.com', 'Shiny'),"web application built on top of R for housing-related data analytics"),
+            br(),
+            
+            h4(HTML('&copy'), ' 2017 by Sorenson Impact Center at the University of Utah')
+            
+        ),
+        uiOutput("projectBox"),
+        uiOutput("companyBox"),
+        uiOutput("houseBox")
+      )
+    ),
+    
     tabItem(tabName = "dashboard",
             fluidRow(
-              box(highchartOutput("plot1", height = 250)),
-              box(plotOutput("plot2", height=250)),
-              box(highchartOutput("plot3", height = 400, width = 500)),
-              box(highchartOutput("plot4", height = 400, width = 500)),
+              column(width=10,
+                     h2("SLC Multifamily: Affordable vs Market"),   
+                     br(),
+                     h4("Datasource from HAND"),
+                     box(highchartOutput("plot1", height = 250), width=NULL)
+              )
+            ),
+            br(),br(), br(),
+            fluidRow(
+              column(width=10,
+                     h2("Unemployment Rate in August 2007-2017"),
+                     br(), br(),
+                     box(plotOutput("plot2", height=250), width=NULL)
+              )
+            ),
+            fluidRow(
+              box(highchartOutput("plot3", height = 400)),
+              box(highchartOutput("plot4", height = 400))
+            ),
+            fluidRow(
+              tags$iframe(src = "http://slcgov.maps.arcgis.com/apps/PublicInformation/index.html?appid=f632417a8bd94d5eb04f1f4eea728ce6", seamless=NA, height = 400, width = "100%")
+            ),
+            fluidRow(
               h3("Median Home Value"),
               h4("This map shows the variation in the median home value in the Salt Lake City area across census tracts. Data is collected from the recent Census"),
               leafletOutput("home", height = 400, width = 1000),
               h3("Median Rent"),
               h4("This map shows the variation in the median gross rent in the Salt Lake City area across census trancts. Data is collected from the recent Census"),
               leafletOutput("rent", height = 400, width = 1000)
-            ),
-            fluidRow(
-              tags$iframe(src = "http://slcgov.maps.arcgis.com/apps/PublicInformation/index.html?appid=f632417a8bd94d5eb04f1f4eea728ce6", seamless=NA, height = 400, width = "100%")
             )
+            
     ),
+    
+    tabItem(tabName = "how",
+            fluidRow(
+              column(width=10,
+                     h2("The growing disparity between wages and rental rates"),   
+                     br(),
+                     h4("A single person household in Salt Lake County has an 
+                        Area Median Income (AMI) of $51,690; the AMI for a family of four is $73,800. 
+                        The graph shows $470 average monthly affordable Affordable gap between affordable
+                        rent for one-person household and 1Br average rent plus utilities, and $610 
+                        average monthly affordable Affordable gap between affordable rent for four-person 
+                        household and 3Br average rent plus utilities."), 
+                     br(),
+                     h4("Salt Lake City Average Rents vs Affordability (80% AMI): Datasource from CBRE 2016"),
+                     box(highchartOutput("graph1", height = 500), width=NULL)
+                     )
+              )
+            ),
     tabItem(tabName = "goals",
             fluidRow(
-             h4("Dynamic Web-based Analytics for Salt Lake City Housing"),
-             withTags({
-               div(class="header", checked=NA,
-                   p("Want to check out the plan?" , a("Click Here",target="_blank",href="plan.pdf"))
-                   
-               )
-             })
+              column(width=10,
+              
+              tags$div(class = "header",
+              h2("Goals of Growing Salt Lake City")),
+              h3("Goal 1: Reform City Practices"),
+              h3("Goal 2:Affordable Housing"),
+              h3("Goal 3:Equitable and fair Housing"),
+              
+              withTags({
+                div(class="header", checked=NA,
+                    p("Want to check out the plan?" , a("Click Here",target="_blank",href="plan.pdf"))
+                    
+                )
+              })
+              )
             )
             
     ),
@@ -110,11 +137,9 @@ body <- dashboardBody(
               infoBox("New Dashboards", 1, icon = icon("dashboard"))
             )
     )
-  
-    
-  )
+    )
+    )
 )
-
 # Put them together into a dashboardPage
 ui <- dashboardPage(
   dashboardHeader(title = "SLC Housing"),
@@ -142,8 +167,8 @@ server <- function(input, output) {
   output$companyBox <-renderUI({
     valueBox(company,
              "Company Profiles",
-              icon = icon("users"),
-              color = "purple")
+             icon = icon("users"),
+             color = "purple")
   })
   
   output$houseBox <- renderUI({
@@ -157,7 +182,7 @@ server <- function(input, output) {
   
   output$plot1<-renderHighchart({
     barchart<-hchart(project_con$`Type (PSH, Affordable, or Market`, 
-                      colorByPoint=TRUE, name="Affordable vs Market in SLC's construction projects")
+                     colorByPoint=TRUE, name="Affordable vs Market in SLC's construction projects")
     print(barchart)
   })
   output$plot2<-renderPlot({
@@ -167,26 +192,26 @@ server <- function(input, output) {
   })
   
   output$plot3<-renderHighchart({
-      permit_plot1<-highchart() %>%
-        hc_chart(type="column") %>%
+    permit_plot1<-highchart() %>%
+      hc_chart(type="column") %>%
       hc_title(text = "New Residential Units in 2017") %>%
       hc_yAxis(title = list(text = "Number of Units")) %>%
       hc_xAxis(categories = c("January", "February", "March", "April", "May", "June",
                               "July", "August", "September")) %>%
       hc_plotOptions(column=list(datalabels = list(enabled = FALSE),
                                  stacking = "normal", enableMouseTracking=TRUE)) %>%
-        
+      
       hc_series(list(name="Single-Family", data=subset(Permit,
-                    `Year of Date` == "2017")$`Single-Family Detached units`),
+                                                       `Year of Date` == "2017")$`Single-Family Detached units`),
                 list(name="Condo/Townhome", data=subset(Permit,
-                    `Year of Date` == "2017")$`Condo/Townhome units`),
+                                                        `Year of Date` == "2017")$`Condo/Townhome units`),
                 list(name="Duplex/Twin Home", data=subset(Permit,
-                    `Year of Date` == "2017")$`Duplex/Twin Home units`),
+                                                          `Year of Date` == "2017")$`Duplex/Twin Home units`),
                 list(name="Apartments", data=subset(Permit,
-                    `Year of Date` == "2017")$`Apartments/ 3 or 4 Family units`
-                    + subset(Permit, `Year of Date` == "2017")$`Apartments/ 5+ Families units`)
-                ) %>%
-    print(permit_plot1)
+                                                    `Year of Date` == "2017")$`Apartments/ 3 or 4 Family units`
+                     + subset(Permit, `Year of Date` == "2017")$`Apartments/ 5+ Families units`)
+      ) %>%
+      print(permit_plot1)
   })
   
   output$plot4<-renderHighchart({
@@ -199,27 +224,42 @@ server <- function(input, output) {
       hc_plotOptions(column=list(datalabels = list(enabled = FALSE),
                                  stacking = "normal", enableMouseTracking=TRUE)) %>%
       hc_series(list(name="Single-Family", data=subset(Permit,
-                    `Year of Date` == "2017")$`Single-Family Detached value`),
+                                                       `Year of Date` == "2017")$`Single-Family Detached value`),
                 list(name="Condo/Townhome", data=subset(Permit,
-                    `Year of Date` == "2017")$`Condo/Townhome value`),
+                                                        `Year of Date` == "2017")$`Condo/Townhome value`),
                 list(name="Duplex/Twin Home", data=subset(Permit,
-                    `Year of Date` == "2017")$`Duplex/Twin Home value`),
+                                                          `Year of Date` == "2017")$`Duplex/Twin Home value`),
                 list(name="Apartments", data=subset(Permit,
-                    `Year of Date` == "2017")$`Apartments/ 3 or 4 Family value`
+                                                    `Year of Date` == "2017")$`Apartments/ 3 or 4 Family value`
                      + subset(Permit, `Year of Date` == "2017")$`Apartments/ 5+ Families value`)
       ) %>%
       print(permit_plot2)
   })
-  
-  
-output$home <- renderLeaflet({
+  ##"how did we get here" output graphs
+  output$graph1<-renderHighchart({
+    affordability1<-highchart() %>%
+      hc_chart(type="column") %>%
+      hc_title(text= "Salt Lake City Average Rents vs Affordability (80% AMI)") %>%
+      hc_xAxis(categories = c("one-person household and 1Br average rent + utilities", 
+                              "four-person household and 3Br average rent + utilities")) %>%
+      hc_series(list(name ="Affordable rent", 
+                     data=c(900, 1300)),
+                list(name = "Average rent",
+                     data=c(1370, 1910))
+      ) %>%
+      print(affordability1)
+  }
+  )
+  output$home <- renderLeaflet({
     home_map
   })
-
-output$rent <- renderLeaflet({
-  rent_map
-})
-
+  
+  output$rent <- renderLeaflet({
+    rent_map
+  })
+  
+  
+  
 }
 
 shinyApp(ui, server)
