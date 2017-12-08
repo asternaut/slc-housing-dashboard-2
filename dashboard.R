2782de2ee1c8330c7688633f5e92d5eaaee6fcc5
Suyash Thite <suyash@solitude.elen.utah.edu>
2017-12-08 12:35:24 -0700

    Removed maps 12/8

--- dashboard.R
+++ dashboard.R
@@ -6,7 +6,7 @@ library(highcharter)
 library(leaflet)
 library(dplyr)
 library(data.table)
-source("tidycensus.R")
+#source("tidycensus.R")
 
 library("treemap")
 library("viridis") 
@@ -131,16 +131,7 @@ body <- dashboardBody(
               h4("How land is zoned has a signifcant effect on how the city develops, and ultimately the supply and demand of housing."),
               tags$iframe(src = "http://slcgov.maps.arcgis.com/apps/PublicInformation/index.html?appid=f632417a8bd94d5eb04f1f4eea728ce6", seamless=NA, height = 400, width = "90%")
             )),
-            fluidRow(
-              column(width = 12,
-              h3("Median Home Value"),
-              h4("This map shows the variation in the median home value in the Salt Lake City area across census tracts. Data is collected from the recent Census"),
-              leafletOutput("home", height = 400, width = 1000),
-              h3("Median Rent"),
-              h4("This map shows the variation in the median gross rent in the Salt Lake City area across census trancts. Data is collected from the recent Census"),
-              leafletOutput("rent", height = 400, width = 1000)
-              )
-            ),
+    
             fluidRow(
               column(width = 12,
                      h2("Salt Lake City Average Rent by Neighborhood"),
--- tidycensus.R
+++ tidycensus.R
@@ -1,58 +1,59 @@
-library("tidyverse")
-library("tidycensus")
-library("ggplot2")
-library("sf")
-library("viridis")
-library("leaflet")
-library("stringr")
+#library("tidyverse")
+#library("tidycensus")
+#library("ggplot2")
+#library("sf")
+#library("viridis")
+#library("leaflet")
+#library("stringr")
 
-census_api_key("d70672fd4eb7640a7bc59b0dbdfa3578a7fc0040")
+#census_api_key("d70672fd4eb7640a7bc59b0dbdfa3578a7fc0040")
 ####code for rent
-utah_rent <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
-                  variables = "B25111_001E", geometry = TRUE)
+#utah_rent <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
+#                  variables = "B25111_001E", geometry = TRUE)
 
-pal <- colorNumeric(palette = "magma", 
-                    domain = utah_rent$estimate)
+#pal <- colorNumeric(palette = "magma", 
+#                    domain = utah_rent$estimate)
 
-rent_map <- utah_rent %>%
-  st_transform(crs = "+init=epsg:4326") %>%
-  leaflet(width = "100%") %>%
-  addProviderTiles(provider = "CartoDB.Positron") %>%
-  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
-              stroke = FALSE,
-              smoothFactor = 0,
-              fillOpacity = 0.7,
-              color = ~ pal(estimate)) %>%
-  addLegend("bottomright", 
-            pal = pal, 
-            values = ~ estimate,
-            title = "Median Gross Rent",
-            labFormat = labelFormat(prefix = "$"),
-            opacity = 1)
+#rent_map <- utah_rent %>%
+#  st_transform(crs = "+init=epsg:4326") %>%
+#  leaflet(width = "100%") %>%
+#  addProviderTiles(provider = "CartoDB.Positron") %>%
+#  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
+#              stroke = FALSE,
+#              smoothFactor = 0,
+#              fillOpacity = 0.7,
+#              color = ~ pal(estimate)) %>%
+#  addLegend("bottomright", 
+#            pal = pal, 
+#            values = ~ estimate,
+#            title = "Median Gross Rent",
+#            labFormat = labelFormat(prefix = "$"),
+#            opacity = 1)
 
 
 
 ###Code for Home Value
-utah_home <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
-                     variables = "B25077_001", geometry = TRUE)
+#utah_home <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
+#                     variables = "B25077_001", geometry = TRUE)
 
-pal <- colorNumeric(palette = "magma", 
-                    domain = utah_home$estimate)
+#pal <- colorNumeric(palette = "magma", 
+#                    domain = utah_home$estimate)
 
-home_map <- utah_home %>%
-  st_transform(crs = "+init=epsg:4326") %>%
-  leaflet(width = "100%") %>%
-  addProviderTiles(provider = "CartoDB.Positron") %>%
-  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
-              stroke = FALSE,
-              smoothFactor = 0,
-              fillOpacity = 0.7,
-              color = ~ pal(estimate)) %>%
-  addLegend("bottomright", 
-            pal = pal, 
-            values = ~ estimate,
-            title = "Median Home Value",
-            labFormat = labelFormat(prefix = "$"),
-            opacity = 1)
+#home_map <- utah_home %>%
+#  st_transform(crs = "+init=epsg:4326") %>%
+#  leaflet(width = "100%") %>%
+#  addProviderTiles(provider = "CartoDB.Positron") %>%
+#  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
+#              stroke = FALSE,
+#              smoothFactor = 0,
+#              fillOpacity = 0.7,
+#              color = ~ pal(estimate)) %>%
+#  addLegend("bottomright", 
+#            pal = pal, 
+#            values = ~ estimate,
+#            title = "Median Home Value",
+#            labFormat = labelFormat(prefix = "$"),
+#            opacity = 1)
 
-home_map
+#home_map
+
