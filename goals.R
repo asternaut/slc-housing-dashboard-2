72fc11ce3a738a05c0e371f560bd52710a224276
puellala <32852287+puellala@users.noreply.github.com>
2018-05-03 11:59:14 -0600

    goals na fixed

--- goals.R
+++ goals.R
@@ -2,22 +2,22 @@ Goals<-read.csv("Data/Goals/Goals Tracker.csv")
 
 #Objective1
 
-Goal11<-read.csv("Data/Goals/Goal1Objective1.csv") %>% replace_na(Progress = " ")
-Goal12 <-read.csv("Data/Goals/Goal1Objective2.csv")
-Goal13 <-read.csv("Data/Goals/Goal1Objective3.csv")
-Goal14 <-read.csv("Data/Goals/Goal1Objective4.csv")
+Goal11<-read.csv("Data/Goals/Goal1Objective1.csv") %>% replace_na(list(Progress = " "))
+Goal12 <-read.csv("Data/Goals/Goal1Objective2.csv") %>% replace_na(list(Progress = " "))
+Goal13 <-read.csv("Data/Goals/Goal1Objective3.csv") %>% replace_na(list(Progress = " "))
+Goal14 <-read.csv("Data/Goals/Goal1Objective4.csv") %>% replace_na(list(Progress = " "))
 
 #objective2
 
-Goal21 <-read.csv("Data/Goals/Goal2Objective1.csv")
-Goal22 <-read.csv("Data/Goals/Goal2Objective2.csv")
-Goal23 <-read.csv("Data/Goals/Goal2Objective3.csv")
-Goal24 <-read.csv("Data/Goals/Goal2Objective4.csv")
-Goal25 <-read.csv("Data/Goals/Goal2Objective5.csv")
-Goal26 <-read.csv("Data/Goals/Goal2Objective6.csv")
+Goal21 <-read.csv("Data/Goals/Goal2Objective1.csv") %>% replace_na(list(Progress = " "))
+Goal22 <-read.csv("Data/Goals/Goal2Objective2.csv") %>% replace_na(list(Progress = " "))
+Goal23 <-read.csv("Data/Goals/Goal2Objective3.csv") %>% replace_na(list(Progress = " "))
+Goal24 <-read.csv("Data/Goals/Goal2Objective4.csv") %>% replace_na(list(Progress = " "))
+Goal25 <-read.csv("Data/Goals/Goal2Objective5.csv") %>% replace_na(list(Progress = " "))
+Goal26 <-read.csv("Data/Goals/Goal2Objective6.csv") %>% replace_na(list(Progress = " "))
 
 
 #objective3
-Goal31 <-read.csv("Data/Goals/Goal3Objective1.csv")
-Goal32 <-read.csv("Data/Goals/Goal3Objective2.csv")
-Goal33 <-read.csv("Data/Goals/Goal3Objective3.csv")
+Goal31 <-read.csv("Data/Goals/Goal3Objective1.csv") %>% replace_na(list(Progress = " "))
+Goal32 <-read.csv("Data/Goals/Goal3Objective2.csv") %>% replace_na(list(Progress = " "))
+Goal33 <-read.csv("Data/Goals/Goal3Objective3.csv") %>% replace_na(list(Progress = " "))
