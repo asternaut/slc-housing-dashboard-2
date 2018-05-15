1c0fbe66d953c35d3c389396bf3d074f59e1eb52
Daniel Hadley <dphnrome@gmail.com>
2018-05-15 12:29:37 -0600

    how I made the map

--- tidycensus.R
+++ tidycensus.R
@@ -33,58 +33,8 @@ opportunity_index_map = leaflet() %>%
             title = "Opportunity Index in Salt Lake City")
 opportunity_index_map
 
+save(opportunity_index_map, file= "./Data/rds/opportunity_index_map.rds")
 
 
 
-
-
-####code for rent
-#utah_rent <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
-#                  variables = "B25111_001E", geometry = TRUE)
-
-#pal <- colorNumeric(palette = "magma", 
-#                    domain = utah_rent$estimate)
-
-#rent_map <- utah_rent %>%
-#  st_transform(crs = "+init=epsg:4326") %>%
-#  leaflet(width = "100%") %>%
-#  addProviderTiles(provider = "CartoDB.Positron") %>%
-#  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
-#              stroke = FALSE,
-#              smoothFactor = 0,
-#              fillOpacity = 0.7,
-#              color = ~ pal(estimate)) %>%
-#  addLegend("bottomright", 
-#            pal = pal, 
-#            values = ~ estimate,
-#            title = "Median Gross Rent",
-#            labFormat = labelFormat(prefix = "$"),
-#            opacity = 1)
-
-
-
-###Code for Home Value
-#utah_home <- get_acs(state = "UT", county = "Salt Lake", geography = "tract", 
-#                     variables = "B25077_001", geometry = TRUE)
-
-#pal <- colorNumeric(palette = "magma", 
-#                    domain = utah_home$estimate)
-
-#home_map <- utah_home %>%
-#  st_transform(crs = "+init=epsg:4326") %>%
-#  leaflet(width = "100%") %>%
-#  addProviderTiles(provider = "CartoDB.Positron") %>%
-#  addPolygons(popup = ~ str_extract(NAME, "^([^,]*)"),
-#              stroke = FALSE,
-#              smoothFactor = 0,
-#              fillOpacity = 0.7,
-#              color = ~ pal(estimate)) %>%
-#  addLegend("bottomright", 
-#            pal = pal, 
-#            values = ~ estimate,
-#            title = "Median Home Value",
-#            labFormat = labelFormat(prefix = "$"),
-#            opacity = 1)
-
-#home_map
 
