8c6346fc1ad18db2694b7fded5b315ff32794206
puellala <32852287+puellala@users.noreply.github.com>
2018-05-15 19:56:52 -0600

    link on how page in bold and first letter in uppercase

--- dashboard.R
+++ dashboard.R
@@ -88,7 +88,7 @@ body <- dashboardBody(
            br()
        )
      ), br(),br(),
-     p(a("Want to Check Out The Plan? Click Here", target="_blank", href="http://www.slcdocs.com/hand/Growing_SLC_Final_Attachments.pdf"))
+     p(a(tags$b("Want to Check Out The Plan? Click Here"), target="_blank", href="http://www.slcdocs.com/hand/Growing_SLC_Final_Attachments.pdf"))
      ),
     
     
@@ -313,7 +313,7 @@ body <- dashboardBody(
                             ),
                     box(width = 6, 
                         h2("Snapshot Salt Lake: Summary", class = "howTitle"),
-                        p("View a snapshot", a("here.", target="_blank", href="Growing_SLC_Snapshot.pdf")),
+                        p(tags$b("View A Snapshot: "), a(tags$b("Here."), target="_blank", href="Growing_SLC_Snapshot.pdf")),
                         p("Data is the key to understanding how our city is growing and developing, what barriers and challenges exist when solving the affordable housing crisis, 
                             and how system design can create a more equitable place to live. This section will focus on the story the data shows about the city’s growth and development 
                             and how that affects the residents of the city. ")
