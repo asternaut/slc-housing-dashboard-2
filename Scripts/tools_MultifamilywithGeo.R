99b1230dd17bd30f2a27aa4e8637c499eb06b4e8
Daniel Hadley <dphnrome@gmail.com>
2018-06-13 10:01:37 -0600

    Cleaning up everything

---
+++ Scripts/tools_MultifamilywithGeo.R
@@ -0,0 +1,10 @@
+library(ggmap)
+
+##### run this assitant tool when we have new Multifamily.xlsx
+
+Multifamily <- read_excel("../Data/Multifamily.xlsx", sheet = "Multi-Family Listings")
+Multifamily$location <- paste0(Multifamily$Address, ", `Salt Lake City`", ", UT ", Multifamily$`Zip Code`)
+geo <- geocode(location = Multifamily$location, output="latlon", source="google")
+Multifamily$lon <- geo$lon
+Multifamily$lat <- geo$lat
+write.csv(Multifamily, "../Data/new_multifamilywithgeo.csv")
